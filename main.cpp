28b33dc2b10ad639003b4f69d71982a3926bf541
virgile-mahaut <60203117+virgile-mahaut@users.noreply.github.com>
2021-02-04 12:18:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "maillage.hpp"
+#include "EF_principal.hpp"
 
 int main()
 {
    Maillage M;
    M.affiche();
+   pf f = affine;
+   vector<double> v =transforme(M,f);
+   for (int i=0; i<M.sommets.size(); i++){
+      cout<<v[i]<<" ";
+   }
+   cout<<endl;
    M.output();
    return 0;
 }
