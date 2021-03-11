cd02080580d2afb47e79fe2fb23a73bcb3320b1d
ZoubairTazakkati <64265945+ZoubairTazakkati@users.noreply.github.com>
2021-03-11 10:48:23 +0100

    Add files via upload

--- elem_finis_res_local.cpp
+++ elem_finis_res_local.cpp
@@ -6,7 +6,7 @@ Maillage resolution_local(Maillage M, Point S1, Point S2, Point S3){
 
 matrice_profil_sym MM(M.sommets.size(),M.sommets.size(),M.P_), 
                        KK(M.sommets.size(),M.sommets.size(),M.P_);
-	pf f = F; pf a = A; pf t_tilde = trace_multi_echelle;
+	pf f = F; pf a = A;
     list<Triangle>::const_iterator itt = M.triangles.begin();
     for (; itt!=M.triangles.end(); itt++){
         // calcul des matrices élémentaires
@@ -23,20 +23,28 @@ matrice_profil_sym MM(M.sommets.size(),M.sommets.size(),M.P_),
             }
         }
     }
-	vecteur FF = transforme_f(M,t_tilde); vecteur LL = - a * KK*FF;
+	vecteur FF = vecteur(M.sommets.size());
+    for (int i=0; i<M.sommets.size(); i++){
+        FF(i+1) = trace_multi_echelle(M.sommets[i].x,M.sommets[i].y, S1, S2, S3);
+    }
+
+    vecteur LL = -1 * KK*FF; 
 	// élimination au bord du domaine
 	for (int I=1; I<=M.sommets.size(); I++){
 		if (M.sommets[I-1].reference == 1){
 			for (int J=I+1; J<=M.sommets.size(); J++){
 				if (I >= M.P_[J-1]) KK(J,I)=0;
 			}
-			KK(I,I)=1; LL(I)=0;
+			KK(I,I)=1;
+            //ATTENTION ICI ON NE FAIT PAS LA PSEUDO ELIMINATION DANS LE MEMBRE DE DROITE
 		}
 	}
 	vecteur UU = resol(KK,LL);
 	for (int i=1; i<=UU.dim();i++){
 		M.sommets[i-1].u = UU(i);
+        if (M.sommets[i].reference == 1)
+		    M.sommets[i-1].u += trace_multi_echelle(M.sommets[i].x,M.sommets[i].y, S1, S2, S3);
 	}
 
-     
+
 }
