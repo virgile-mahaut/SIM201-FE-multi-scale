ea42bf41a64fb47caa1e54b3aa51e9552e6568fb
ZoubairTazakkati <64265945+ZoubairTazakkati@users.noreply.github.com>
2021-03-11 09:33:35 +0100

    Update EF_principal.hpp

--- EF_principal.hpp
+++ EF_principal.hpp
@@ -4,5 +4,7 @@
 
 double A(double x, double y);
 double F(double x, double y);
+double trace_multi_echelle (double x, double y, Point S1, Point S2, Point S3)
+double sol_exact_validation(double x, double y);
 
 #endif
